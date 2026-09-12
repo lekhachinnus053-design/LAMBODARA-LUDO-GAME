bab6eaab55094f520a9192d607c839e891e1ec46
lekhachinnus053-design <lekhachinnus053@gmail.com>
2026-09-12 23:35:38 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="Manual Ludo Game",
+    page_title="LAMBODARA LUDO GAME",
     page_icon="🎲",
     layout="wide"
 )
